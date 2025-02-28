73a375be27aae40d6e0d9ca12af3c46f5666bb54
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-02-28 10:30:59 +0200

    05 completed

---
+++ JS Syntax Fundamentals - Exercises/05. Multiplication Table.js
@@ -0,0 +1,15 @@
+function multiplication(number) {   
+
+    for (let a = 1; a <= 10; a++) {
+
+        let times = a
+        let product = number * times
+        console.log(`${number} X ${times} = ${product}`)
+
+    }
+
+
+}
+
+multiplication(5)
+multiplication(2)
