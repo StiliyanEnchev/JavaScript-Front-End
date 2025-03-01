31330e5d86d8a905c84ec1ead5211cd0ce7379d9
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-03-01 11:17:59 +0200

    01 completed

---
+++ JS Arrays and Strings - Lab/1. Sum First and Last Array Elements.js
@@ -0,0 +1,6 @@
+function sum_elements(masiv) {
+    let result = masiv[0] + masiv[masiv.length - 1]
+    console.log(result)
+}
+
+sum_elements([20, 30, 40])
---
+++ JS Arrays and Strings - Lab/2. Reverse an Array of Numbers.js
@@ -0,0 +1,5 @@
+function solve(num, masiv) {
+    
+}
+
+solve(3, [10, 20, 30, 40, 50])
