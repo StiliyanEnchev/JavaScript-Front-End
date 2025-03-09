7f11075af5495158adcf613f38f3e37ec7032505
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-03-09 12:18:27 +0200

    03 completed

---
+++ JS Functions and Statements - Exercises/03_characters_in_range.js
@@ -0,0 +1,17 @@
+function asciTable (firstSym, SecondSym) {
+    let index1 = firstSym.charCodeAt();
+    let index2 = SecondSym.charCodeAt();
+    let result = [];
+    if (index2 < index1) {
+        index1 = SecondSym.charCodeAt();
+        index2 = firstSym.charCodeAt();
+        }
+        
+    for (index1; index1 < index2 - 1; index1 ++) {
+        result.push(String.fromCharCode(index1 + 1));
+    }
+    console.log(result.join(' '))
+}
+
+asciTable('a', 'd')
+asciTable('c', "#")
