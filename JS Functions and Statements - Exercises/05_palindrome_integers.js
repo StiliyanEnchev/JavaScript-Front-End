1b1689c95efc1b31eb4ab6e2267c1de89ffb335e
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-03-10 18:16:42 +0200

    05 completed

---
+++ JS Functions and Statements - Exercises/05_palindrome_integers.js
@@ -0,0 +1,12 @@
+function isPalindrome(num) {
+    for (number of num) {
+        const str = number.toString();  
+        console.log(str === str.split("").reverse().join(""));
+
+
+    }
+}
+
+
+
+isPalindrome([123, 323, 421, 121])
