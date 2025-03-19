b201a13e70f5f8a2dfbc47a717d01656b89aac70
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-03-19 14:44:35 +0200

    01 completed

---
+++ objectsAndClassesExercise/01_Employees.js
@@ -0,0 +1,15 @@
+function solve(arr) {
+    let obj = []
+    
+    for (row of arr) {
+        console.log(`Name: ${row} -- Personal Number: ${row.length}`)
+    }
+}
+
+solve([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+    ]
+    )
