ffd55ae86a169f1134a9812ed321d8e5d1af5fd2
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-03-22 11:13:03 +0200

    06 completed

--- 05.JS-Front-End-DOM-Introduction-Lab-Resources/06.Sum Table/sum-table.js
+++ 05.JS-Front-End-DOM-Introduction-Lab-Resources/06.Sum Table/sum-table.js
@@ -1,3 +1,19 @@
 function sumTable() {
-    // TODO
+    let rows = [...document.querySelectorAll('table tr')];
+    rows.shift();
+    let output = rows.pop();
+
+    let sum = 0;
+    
+    for (row of rows) {
+        let col = row.children[row.children.length - 1];
+        sum += Number(col.textContent);
+    }
+
+    let outputCol = document.getElementById('sum');
+    outputCol.textContent = sum
+
+
+
+
 }
