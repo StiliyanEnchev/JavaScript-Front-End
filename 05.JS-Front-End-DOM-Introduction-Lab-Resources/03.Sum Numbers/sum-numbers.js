d4de8221b0baf0d72d82c7e1f07037196b4b35f9
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-03-22 10:38:29 +0200

    03 completed

--- 05.JS-Front-End-DOM-Introduction-Lab-Resources/03.Sum Numbers/sum-numbers.js
+++ 05.JS-Front-End-DOM-Introduction-Lab-Resources/03.Sum Numbers/sum-numbers.js
@@ -1,3 +1,5 @@
 function calc() {
-    // TODO
+    let input1 = Number(document.getElementById("num1").value);
+    let input2 = Number(document.getElementById("num2").value);
+    document.getElementById('sum').value = String((input1 + input2))
 }
