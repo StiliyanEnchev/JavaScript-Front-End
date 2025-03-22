8a112087ee5ae6463620d1e95a3e60c686765f6b
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-03-22 09:57:31 +0200

    02 completed

--- 05.JS-Front-End-DOM-Introduction-Lab-Resources/02.Collect List Items/collect-list-items.js
+++ 05.JS-Front-End-DOM-Introduction-Lab-Resources/02.Collect List Items/collect-list-items.js
@@ -1,3 +1,7 @@
 function extractText() {
-    // TODO
+    let items = Array(document.getElementById('items'));
+    
+    for (item of items) {
+        document.getElementById('result').textContent = item.textContent
+    }
 }
