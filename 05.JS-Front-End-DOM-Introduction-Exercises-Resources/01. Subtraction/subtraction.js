c06640d06014be5fb4c98baa57d154d5765239f7
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-03-23 10:35:37 +0200

    01 completed

--- 05.JS-Front-End-DOM-Introduction-Exercises-Resources/01. Subtraction/subtraction.js
+++ 05.JS-Front-End-DOM-Introduction-Exercises-Resources/01. Subtraction/subtraction.js
@@ -1,3 +1,9 @@
 function subtract() {
-    //TODO
+    let firstNumber = document.getElementById('firstNumber').value;
+    let secondNumber = document.getElementById('secondNumber').value;
+    let result = document.getElementById('result');
+    console.log(Number(firstNumber))
+    console.log(Number(secondNumber))
+
+    result.textContent += result.textContent + String((Number(firstNumber) - Number(secondNumber)))
 }
