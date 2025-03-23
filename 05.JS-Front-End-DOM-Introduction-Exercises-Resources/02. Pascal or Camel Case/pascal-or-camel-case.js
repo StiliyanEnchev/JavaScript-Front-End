95472fec657277a0aab6c35656dab50c7cb109ae
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-03-23 11:04:40 +0200

    02 completed

--- 05.JS-Front-End-DOM-Introduction-Exercises-Resources/02. Pascal or Camel Case/pascal-or-camel-case.js
+++ 05.JS-Front-End-DOM-Introduction-Exercises-Resources/02. Pascal or Camel Case/pascal-or-camel-case.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO
+  let text = document.getElementById('text').value;
+  let convention = document.getElementById('naming-convention').value;
+  let result = document.getElementById('result')
+
+  function toLowerCamelCase(str) {
+    let words = toCamelCase(str);
+    return words[0].toLowerCase() + words.slice(1);
+  }
+  function toCamelCase(str) {
+    return str.split(' ')
+      .map(word => word[0].toUpperCase() + word.slice(1).toLowerCase())
+      .join('');
+  }
+
+  if (convention == 'Camel Case') {
+    result.textContent += toLowerCamelCase(text)
+  } else if (convention == 'Pascal Case') {
+    result.textContent += toCamelCase(text)
+  } else {
+    result.textContent += 'Error!'
+  }
+
 }
