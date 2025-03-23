1e1211eabe097cf1d44a8964e3b7da7abec81f85
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-03-23 11:54:32 +0200

    03 completed

--- 05.JS-Front-End-DOM-Introduction-Exercises-Resources/03. Accordion/accordion.js
+++ 05.JS-Front-End-DOM-Introduction-Exercises-Resources/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    //TODO
+    let content = document.getElementById('extra');
+    let btn = document.querySelector('.button');
+
+    if (btn.textContent == 'More') {
+        content.style.display = 'block'
+        btn.textContent = 'Less'
+    } else {
+        content.style.display = 'none'
+        btn.textContent = 'More'
+    }
 }
