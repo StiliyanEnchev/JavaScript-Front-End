b380bf6f6911a39ccf8cf29fb378bab0afb7a69b
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-03-25 09:14:17 +0200

    04 completed

--- 05.JS-Front-End-DOM-Introduction-Exercises-Resources/04. Search in List/search-in-list.js
+++ 05.JS-Front-End-DOM-Introduction-Exercises-Resources/04. Search in List/search-in-list.js
@@ -1,3 +1,19 @@
 function solve() {
-   // TODO
+    let input = document.getElementById('searchText');
+    let list = document.getElementById('towns');
+    let output = document.getElementById('result');
+    let number = 0;
+
+    for (el of list.children) {
+        if (el.textContent.includes(input.value)) {
+            el.style.fontWeight = 'bold';
+            el.style.textDecoration = 'underline'
+            number++;
+        } else {
+            el.style.fontWeight = '';
+            el.style.textDecoration = '';
+        }
+    }
+
+    output.textContent = `${number} matches found`
 }
