8c822360c57e61b43703df49e64d23b73d6720d2
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-03-25 12:24:22 +0200

    06 completed

--- 05.JS-Front-End-DOM-Introduction-Exercises-Resources/06. Format the Text/format-the-text.js
+++ 05.JS-Front-End-DOM-Introduction-Exercises-Resources/06. Format the Text/format-the-text.js
@@ -1,3 +1,43 @@
 function solve() {
-  // TODO
+  let input = document.getElementById('input').value;
+  let output = document.getElementById('output');
+  splitted = input.split('.');
+  result = '';
+  finalString = '';
+
+  if (splitted.length <= 2) {
+    for (sentence of splitted) {
+      if (sentence.length > 0) {
+
+        result += `${sentence}.`;
+      }
+    }
+  } else {
+    for (let i = 0; i < splitted.length; i++) {
+
+      if (splitted[i].length > 0) {
+
+        result += `${splitted[i]}.`;
+        if ((i + 1) % 3 == 0) {
+          finalString += `<p>${result}</p>`;
+          result = '';
+        }
+      }
+    }
+  }
+
+  if (result.length > 0) {
+    finalString += `<p>${result}</p>`;
+
+  }
+  output.innerHTML = finalString;
+
 }
+
+
+
+
+
+
+
+
