af828d7e2f9a00056f8548b018768503c78bc33b
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-04-01 12:37:50 +0300

    update

--- DOM Manipulation and Events - Lab/01. List-Of-Items/list-of-items.js
+++ DOM Manipulation and Events - Lab/01. List-Of-Items/list-of-items.js
@@ -2,6 +2,8 @@ function addItem() {
     let element = document.getElementById('newItemText');
     let listItems = document.getElementById("items");
     let newListItem = document.createElement('li');
-    newListItem.textContent = element.value;
-    listItems.appendChild(newListItem);
+    if (element.value) {
+        newListItem.textContent = element.value;
+        listItems.appendChild(newListItem);
+    }
 }
