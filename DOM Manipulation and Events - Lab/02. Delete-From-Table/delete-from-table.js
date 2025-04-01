75e8077d2f4926e8f2511b09e212fd0ee0fac700
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-04-01 14:26:56 +0300

    02 completed

--- DOM Manipulation and Events - Lab/02. Delete-From-Table/delete-from-table.js
+++ DOM Manipulation and Events - Lab/02. Delete-From-Table/delete-from-table.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    
+    let input = document.querySelector('[name="email"]');
+    let pattern = input.value;
+    let result = document.getElementById('result');
+
+    let rows = Array.from(document.querySelectorAll('tbody tr'));
+
+    for (let row of rows) {
+        let emailCol = row.children[1]
+        if (emailCol.textContent == pattern) {
+            row.remove();
+            result.textContent = 'Deleted'
+        } else {
+            result.textContent = 'Not found.'
+
+        }
+    }
 }
