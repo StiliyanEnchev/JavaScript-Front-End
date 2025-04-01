8e61194c0931bb67e7a1961904a9b43feee9a93e
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-04-01 11:26:56 +0300

    08 completed

--- 05.JS-Front-End-DOM-Introduction-Exercises-Resources/08. Generate Report/generate-report.js
+++ 05.JS-Front-End-DOM-Introduction-Exercises-Resources/08. Generate Report/generate-report.js
@@ -1,3 +1,33 @@
 function solve() {
-    //TODO
+    let selected = new Map();
+
+    let headers = document.querySelectorAll('thead [type="checkbox"]');
+
+    for (let i = 0; i < headers.length; i++) {
+        if (headers[i].checked) {
+            selected.set(i, headers[i].name)
+        }
+    }
+
+    let rows = document.querySelectorAll('tbody tr');
+    let result = [];
+    for (row of rows) {
+
+        let record = {};
+
+        for (let i = 0; i < row.children.length; i++) {
+            if (selected.has(i)) {
+                let propName = selected.get(i);
+                let content = row.children[i].textContent;
+                record[propName] = content;
+            }
+        }
+
+        result.push(record);
+
+    }
+
+    let output = document.getElementById('output');
+    output.value = JSON.stringify(result)
+
 }
