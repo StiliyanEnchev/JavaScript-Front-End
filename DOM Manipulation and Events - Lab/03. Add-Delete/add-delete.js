ca71dcc4353abadf305d4fd24bab748708dbbf1b
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-04-01 16:53:35 +0300

    03 completed

--- DOM Manipulation and Events - Lab/03. Add-Delete/add-delete.js
+++ DOM Manipulation and Events - Lab/03. Add-Delete/add-delete.js
@@ -1,3 +1,22 @@
 function addItem() {
-    // TODO
+    let element = document.getElementById('newItemText');
+    let listItems = document.getElementById("items");
+    let newListItem = document.createElement('li');
+    if (element.value) {
+        newListItem.textContent = element.value;
+        listItems.appendChild(newListItem);
+    }
+    
+    let deleteBtn = document.createElement('a');
+    deleteBtn.href = '#';
+    deleteBtn.textContent = '[Delete]';
+    deleteBtn.addEventListener('click', onDelete);
+
+    newListItem.appendChild(deleteBtn);
+
+    function onDelete(e) {
+        let item = e.target.parentElement;
+        item.remove();
+    }
+
 }
