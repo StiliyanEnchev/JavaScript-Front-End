4c88c3a50707d9a492319666df027352c9966f94
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-04-02 11:22:23 +0300

    05 completed

--- DOM Manipulation and Events - Lab/05. Highlight-Active/hightlight-active.js
+++ DOM Manipulation and Events - Lab/05. Highlight-Active/hightlight-active.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', focused);
 
 function focused() {
-    //TODO
+    let inputs = document.querySelectorAll('input');
+
+    for (let input of inputs) {
+        input.addEventListener('focus', onFocus);
+        input.addEventListener('blur', onBlur)
+
+    }
+
+    function onFocus(event) {
+        event.target.parentElement.classList.add('focused');
+    }
+
+    function onBlur(event) {
+        event.target.parentElement.classList.remove('focused')
+    }
 }
+
