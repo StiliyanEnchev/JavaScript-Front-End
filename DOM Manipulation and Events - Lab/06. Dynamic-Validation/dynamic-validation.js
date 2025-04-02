d19d454e765808d5b01fdb0a3f6b463e74b4fd1d
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-04-02 12:00:59 +0300

    07 completed

--- DOM Manipulation and Events - Lab/06. Dynamic-Validation/dynamic-validation.js
+++ DOM Manipulation and Events - Lab/06. Dynamic-Validation/dynamic-validation.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    let emailInput = document.getElementById('email');
+
+    emailInput.addEventListener('change', () => {
+        let emailPattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+        if (emailInput.value.match(emailPattern)) {
+            emailInput.classList.remove('error');
+        } else {
+            emailInput.classList.add('error');
+        }
+    });
 }
