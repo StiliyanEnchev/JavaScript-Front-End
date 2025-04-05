098b14a878b3fe2919f113c6bac28264fc842c8a
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-04-05 13:15:42 +0300

    04 completed

--- DOM Manipulation and Events - Exercise/04. Fill-Dropdown/fill-dropdown.js
+++ DOM Manipulation and Events - Exercise/04. Fill-Dropdown/fill-dropdown.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    let form = document.querySelector('form');
+    let menu = document.getElementById('menu');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        newItemValue = document.getElementById('newItemValue');
+        newItemText = document.getElementById('newItemText');
+        
+        let newMenuItem = document.createElement('option');
+        newMenuItem.value = newItemValue.value;
+        newMenuItem.textContent = newItemText.value;
+        menu.appendChild(newMenuItem)
+        newItemText.value = '';
+        newItemValue.value = '';
+    })
 }
