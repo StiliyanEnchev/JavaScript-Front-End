36f82b7fee7171469ba05830bd30f5c952fcc642
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-04-05 11:11:35 +0300

    02 completed

--- DOM Manipulation and Events - Exercise/02. Time-Converter/time-converter.js
+++ DOM Manipulation and Events - Exercise/02. Time-Converter/time-converter.js
@@ -1,5 +1,36 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    let main = document.querySelector('main');
+
+    main.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        let form = event.target;
+        let id = form.id;
+
+        let daysInput = document.getElementById('days-input');
+        let hoursInput = document.getElementById('hours-input');
+        let minutesInput = document.getElementById('minutes-input');
+        let secondsInput = document.getElementById('seconds-input'); 
+
+        let days = 0;
+
+        if (id === 'days') {
+            days = parseFloat(daysInput.value);
+        } else if (id === 'hours') {
+            days = parseFloat(hoursInput.value) / 24;
+        } else if (id === 'minutes') {
+            days = parseFloat(minutesInput.value) / 1440;
+        } else if (id === 'seconds') {
+            days = parseFloat(secondsInput.value) / 86400;
+        }
+
+        daysInput.value = days.toFixed(2);
+        hoursInput.value = (days * 24).toFixed(2);
+        minutesInput.value = (days * 1440).toFixed(2);
+        secondsInput.value = (days * 86400).toFixed(2);
+
+    
+    })
 }
