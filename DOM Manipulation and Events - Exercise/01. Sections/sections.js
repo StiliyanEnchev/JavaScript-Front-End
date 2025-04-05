f2230449180282f121f8b7e83eaa610bf6f4f38c
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-04-05 10:42:12 +0300

    01 completed

--- DOM Manipulation and Events - Exercise/01. Sections/sections.js
+++ DOM Manipulation and Events - Exercise/01. Sections/sections.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-   // TODO
+   const form = document.getElementById('task-input');
+   const inputField = form.querySelector('input[type="text"]');
+   let content = document.getElementById('content');
+
+   form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      const receivedText = inputField.value;
+      sections = receivedText.split(', ')
+      for (sec of sections) {
+         const div = document.createElement('div');
+         const p = document.createElement('p');
+         p.textContent = sec;
+         div.append(p);
+         content.appendChild(div);
+      }
+   })
 }
+
+
+
+
+
+
