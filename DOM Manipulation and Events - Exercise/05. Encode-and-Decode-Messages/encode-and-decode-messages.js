a655c7aeb7623ca2f6320b909ec9830a1d8360d3
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-04-05 15:44:28 +0300

    05 completed

--- DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
+++ DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/encode-and-decode-messages.js
@@ -1,5 +1,42 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    let encode = document.getElementById('encode');
+    let decode = document.getElementById('decode');
+
+    let encodeInput = encode.querySelector('textarea');
+    let decodeInput = decode.querySelector('textarea');
+
+
+    encode.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        current_message = encodeInput.value;
+        let encoded = '';
+
+        for (let char of current_message) {
+            encoded += String.fromCharCode(char.charCodeAt(0) + 1);
+        }
+
+        decodeInput.value = encoded;
+        encodeInput.value = '';
+
+    })
+
+    decode.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        current_message = decodeInput.value;
+        let decoded = '';
+
+        for (let char of current_message) {
+            decoded += String.fromCharCode(char.charCodeAt(0) - 1);
+        }
+        
+        decodeInput.value = decoded;
+
+
+
+    })
+    
 }
