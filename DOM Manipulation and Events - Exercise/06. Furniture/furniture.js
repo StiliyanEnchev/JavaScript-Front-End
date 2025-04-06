070d1e14602984133034684cbaf5c4c596c3968a
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-04-06 11:45:03 +0300

    07 started

--- DOM Manipulation and Events - Exercise/06. Furniture/furniture.js
+++ DOM Manipulation and Events - Exercise/06. Furniture/furniture.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-  //TODO
+    let [generateBtn, buyBtn] = Array.from(document.querySelectorAll('[type="submit"]'));
+    generateBtn.addEventListener('click', onGenerate);
+    buyBtn.addEventListener('click', onBuy);
+
+    function onGenerate(ev) {
+        ev.preventDefault();
+        recValue = document.querySelector('textarea');
+        console.log(typeof(recValue))
+
+    }
+
+    function onBuy(ev) {
+        ev.preventDefault();
+    }
 }
+
