e5bdb91de7d08287b30a6ff692e8d7e80c73fa90
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-04-07 16:48:58 +0300

    02 completed

--- 08.Asynchronous-Programming-Lab-Resources/01.Bus-Stop/app.js
+++ 08.Asynchronous-Programming-Lab-Resources/01.Bus-Stop/app.js
@@ -1,3 +1,49 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    let stopId = document.getElementById('stopId').value;
+
+    let info;
+
+    try {
+        info = await getBusInfo(stopId);
+    } catch (err) {
+        onError();
+
+        return;
+    }
+
+    document.getElementById('stopName').textContent = info.name;
+
+    let list = document.getElementById('buses');
+
+    list.replaceChildren();
+
+    for (let busId in info.buses) {
+        let item = document.createElement('li');
+        let time = info.buses[busId];
+        item.textContent = `Bus ${busId} arrives in ${time} minutes`;
+
+        list.appendChild(item);
+    }
+
+    function onError(err) {
+        document.getElementById('stopName').textContent = "Error"
+    }
+
+    console.log(info)
+
+    async function getBusInfo(busId) {
+        let url = `http://localhost:3030/jsonstore/bus/businfo/${busId}`
+
+        let response = await fetch(url);
+
+        if (response.status == 204) {
+            throw new Error('No info for stop' + stopId);
+
+        }
+
+        let data = await response.json();
+
+        return data;
+
+    }
 }
