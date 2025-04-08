2f91f4727a02a88a37f6b241413145d653ba8805
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-04-08 19:15:49 +0300

    06 completed

--- DOM Manipulation and Events - Exercise/06. Furniture/furniture.js
+++ DOM Manipulation and Events - Exercise/06. Furniture/furniture.js
@@ -7,13 +7,58 @@ function solve() {
 
     function onGenerate(ev) {
         ev.preventDefault();
-        recValue = document.querySelector('textarea');
-        console.log(typeof(recValue))
+        recValue = document.querySelector('textarea').value;
+        const data = JSON.parse(recValue);
+        const tbody = document.querySelector('tbody');
+
+        for (let item of data) {
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+        <td><img src="${item.img}"></td>
+        <td><p>${item.name}</p></td>
+        <td><p>${item.price}</p></td>
+        <td><p>${item.decFactor}</p></td>
+        <td><input type="checkbox"></td>
+        `;
+            tbody.appendChild(tr)
+        }
+
 
     }
 
     function onBuy(ev) {
         ev.preventDefault();
+        textAreaOutput = document.getElementsByTagName('textarea');
+        let output = textAreaOutput[1];
+        const tbody = document.querySelector('tbody');
+        const rows = tbody.getElementsByTagName('tr');
+        let boughtList = [];
+        let totalPrice = 0;
+        let factors = [];
+
+        for (let item of rows) {
+            const columns = item.getElementsByTagName('td');
+
+            if (columns[4].querySelector('input').checked) {
+                const name = columns[1].textContent;
+                const price = columns[2].textContent;
+                const decFactor = columns[3].textContent;
+                boughtList.push(name)
+                totalPrice += Number(price);
+                factors.push(Number(decFactor))
+            }
+
+
+        }
+
+        let sum = 0;
+        for (let num of factors) sum += num;
+        let average = sum / factors.length;
+
+        output.value +=  `Bought furniture: ${boughtList.join(', ')}`
+        output.value += `\nTotal price: ${totalPrice}`
+        output.value += `\nAverage decoration factor: ${average}`
+
     }
 }
 
