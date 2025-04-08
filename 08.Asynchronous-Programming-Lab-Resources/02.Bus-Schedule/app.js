c55e2a1e468e96251f33a925335af8683f1ac9f1
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-04-08 20:04:19 +0300

    02 completed

--- 08.Asynchronous-Programming-Lab-Resources/02.Bus-Schedule/app.js
+++ 08.Asynchronous-Programming-Lab-Resources/02.Bus-Schedule/app.js
@@ -1,17 +1,50 @@
 function solve() {
+    
 
-    function depart() {
-        console.log('Depart TODO...');
+    async function depart() {
+        let stopID = 'depot'
+        let nextStop = await getNextStop(stopID);
+
+
+        let info = document.getElementById('info');
+        let depBtn = document.getElementById('depart');
+        let arrBtn = document.getElementById('arrive');
+
+        info.textContent = `Next stop ${nextStop.name}`;
+        depBtn.disabled = true;
+        arrBtn.disabled = false;
     }
 
-    function arrive() {
-        console.log('Arrive TODO...');
+    async function arrive() {
+        let stopID = 'depot'
+        let nextStop = await getNextStop(stopID);
+
+        let info = document.getElementById('info');
+        let depBtn = document.getElementById('depart');
+        let arrBtn = document.getElementById('arrive');
+
+        info.textContent = `Arriving at ${nextStop.name}`;
+        depBtn.disabled = false;
+        arrBtn.disabled = true;
     }
 
     return {
         depart,
         arrive
     };
+}
+
+async function getNextStop(stopID) {
+    let res = await fetch('http://localhost:3030/jsonstore/bus/schedule/' + stopID);
+
+    if (!res.ok) {
+        alert('Request error');
+        throw new Error('Request error');
+    }
+
+    let data = await res.json();
+
+    return data;
 }
 
 let result = solve();
