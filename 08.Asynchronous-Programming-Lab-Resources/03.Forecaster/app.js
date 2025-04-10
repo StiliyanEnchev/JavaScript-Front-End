8cd375e79e62a5a7f694b869f57bf91e0b735aa6
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-04-10 09:56:01 +0300

    initial update

--- 08.Asynchronous-Programming-Lab-Resources/03.Forecaster/app.js
+++ 08.Asynchronous-Programming-Lab-Resources/03.Forecaster/app.js
@@ -1,5 +1,28 @@
 function attachEvents() {
-    console.log("TODO...");
+    let submitButton = document.getElementById('submit');
+    let forecastSection = document.getElementById('forecast');
+    let currentConditions = document.getElementById('current');
+    let upcomingForecast = document.getElementById('upcoming');
+
+    submitButton.addEventListener('click', getLocation);
+
+    async function getLocation() {
+        let locationInput = document.getElementById('location');
+        let loc = await fetch('http://localhost:3030/jsonstore/forecaster/locations/')
+
+        if (!loc.ok) {
+            alert('Request error');
+            throw new Error('Request error');
+        }
+
+        let data = await loc.json();
+
+        for (let city of data) {
+            if (city.code == locationInput.value) {
+                console.log(locationInput.value)
+            }
+        }
+    }
 }
 
 attachEvents();
